005303a2945bdd229243a6796c7d3bd1048f3eff
KuiLiao <horsekiao@gmail.com>
2022-05-18 23:10:26 +0800

    add server center plugin

---
+++ eureka-plugin/src/main/java/pub/dtm/client/configuration/EurekaConfiguration.java
@@ -0,0 +1,11 @@
+package pub.dtm.client.configuration;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author horseLk
+ * @date 2022-05-18 22:40
+ */
+@EnableEurekaClient
+public class EurekaConfiguration {
+}
